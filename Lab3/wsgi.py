0074bcd9e05d95203971fe7adc18636dd7055526
unknown <>
2015-11-06 11:21:36 +0800

    lab

--- Lab3/wsgi.py
+++ Lab3/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+#import os
+#import sys
+#root = os.path.dirname(__file__)
+#sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Lab3.settings")
+#
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
 import os
-import sys
-root = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Lab3.settings")
 
 from django.core.wsgi import get_wsgi_application
